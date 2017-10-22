35c5863ca9424d735b915241b4ca61e4a7077ecb
yennanliue <f339339@gmail.com>
2017-10-22 18:20:45 +0100

    fix exported route

--- init.sh
+++ init.sh
@@ -2,7 +2,7 @@
 
 
 
-#export PYTHONPATH=/Users/yennanliu/KKBox_Music_Recommendation
 route=$(pwd)
 export PYTHONPATH=$route
 echo 'add current route to python path...'
+#export PYTHONPATH=/Users/yennanliu/KKBox_Music_Recommendation
