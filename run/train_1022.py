f8833152e967d727b7ac168ccb5788cdafee10b9
yennanliue <f339339@gmail.com>
2017-10-22 18:22:42 +0100

    test

--- run/train_1022.py
+++ run/train_1022.py
@@ -4,9 +4,16 @@
 import numpy as np
 import pandas as pd
 
-
 from script.utility_data_preprocess import *
 
 
-#train, test, songs, members, song_extra_info = load_data()
-load_data_()
+def prepare():
+	train, test, songs, members, song_extra_info = load_data()
+	members_ = get_time_feature(members)
+
+
+if __name__ == '__main__':
+	#train, test, songs, members, song_extra_info = load_data()
+	load_data_test()
+
+
--- script/utility_data_preprocess.py
+++ script/utility_data_preprocess.py
@@ -6,15 +6,15 @@ import numpy as np
 import pandas as pd
 
 
-def load_data_():
-	data_path = '../data/'
-	song_extra_info =  pd.read_csv(data_path + 'song_extra_info.csv')
+def load_data_test():
+	data_path = '/Users/yennanliu/KKBox_Music_Recommendation/data/'
+	song_extra_info =  pd.read_csv(data_path + 'song_extra_info.csv',nrows=10)
 	print (song_extra_info.head(10))
 	return  song_extra_info
 
 
 def load_data():
-	data_path = '../data/'
+	data_path = '/Users/yennanliu/KKBox_Music_Recommendation/data/'
 	train = pd.read_csv(data_path + 'train.csv')
 	test = pd.read_csv(data_path + 'test.csv')
 	songs = pd.read_csv(data_path + 'songs.csv')
